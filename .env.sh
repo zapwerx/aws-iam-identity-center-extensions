3424a942d4d8972eb16d6e235701a19f960ed744
Andrew Gillis <gillis.andrew@gmail.com>
2025-05-07 18:44:06 +0000

    workshop deployment

---
+++ .env.sh
@@ -0,0 +1,21 @@
+export DEPLOYMENT_ACCOUNT='116862854202'
+export DEPLOYMENT_PROFILE=zapwerx-org
+export DEPLOYMENT_REGION=us-east-1
+export TARGET_ACCOUNT='116862854202'
+export TARGET_PROFILE=zapwerx-org
+export TARGET_REGION=us-east-1
+export ORGMAIN_ACCOUNT='116862854202' 
+export ORGMAIN_PROFILE=zapwerx-org
+export ORGMAIN_REGION=us-east-1
+export SSO_ACCOUNT='116862854202' 
+export SSO_PROFILE=zapwerx-org
+export SSO_REGION=us-east-1
+export AWS_PROFILE=zapwerx-org
+export AWS_REGION=us-east-1
+export BOOTSTRAP_QUALIFIER="ssoutility"
+export CFN_EXECUTION_POLICIES="arn:aws:iam::aws:policy/AdministratorAccess"
+export CONFIG="env"
+
+# arn:aws:iam::116862854202:role/LinkCallerRole
+# aws:iam::116862854202:role/PermissionSetCallerRole
+# 
